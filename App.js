8c745ab50acfe5ae7d12ecee2373e4403c8ba6a1
Artem Vasilenko <versuscolt@gmail.com>
2022-06-30 18:49:03 +0300

    Was fixed a bug

--- App.js
+++ App.js
@@ -2,8 +2,11 @@ import React from 'react';
 import {useState, useEffect} from 'react';
 import {
   View,
+  Alert
 } from 'react-native';
 import NetInfo from '@react-native-community/netinfo/';
+
+import axios from 'axios';
 
 import {get} from './src/services/request';
 import {Loading} from './src/screens/loading/Loading';
@@ -14,23 +17,72 @@ import {endpoint} from './src/constants';
 
 const App = () => {
   const [state, setState] = useState(<Loading />);
+  // const [request, setRequest] = useState(false);
+
+  const getRequest = async (url) => {
+    try {
+      const response = await axios.get(url);
+      // console.log(response)
+      setState(<Site />);
+    } catch (error) {
+      // console.log(error)
+      setState(<Game />)
+    }
+  }
 
   useEffect(() => {
+    // NetInfo.fetch().then(data => {
+    //   if (data.isConnected) {
+    //     axios.get(endpoint)
+    //     .then(function(response) {
+    //       // console.log(response.status);
+    //       setState(<Site />);
+    //     })
+    //     .catch(function(error) {
+    //       // console.log(error)
+    //       setState(<Game />)
+    //     })
+    //   } else {
+    //     setState(<Game />);
+    //   }
+    // })
     NetInfo.fetch().then(data => {
       if (data.isConnected) {
-        get(endpoint)
-          .then(response => {
-            if (response.status === 200) {
-              setState(<Site />);
-            } else {
-              setState(<Game />);
-            }
-        });
+        getRequest(endpoint);
       } else {
         setState(<Game />);
       }
-    });
+    })
+    // if (request) {
+    //   axios.get(endpoint)
+    //     .then(function(response) {
+    //       console.log(response.status);
+    //       setState(<Site />);
+    //     })
+    //     .catch(function(error) {
+    //       console.log(error)
+    //       setState(<Game />)
+    //     })
+    // }
+    // getRequest(endpoint)
   }, []);
+
+  // NetInfo.fetch().then(data => {
+  //   // Alert.alert('Connecting is: ' + data.isConnected);
+  //   if (data.isConnected) {
+  //     get(endpoint)
+  //       .then(response => {
+  //         // Alert.alert('Request status: ' + response.status);
+  //         if (response.status === 200) {
+  //           setState(<Site />);
+  //         } else {
+  //           setState(<Game />);
+  //         }
+  //     });
+  //   } else {
+  //     setState(<Game />);
+  //   }
+  // });
 
   return (
     <View style={{flex: 1}}>
